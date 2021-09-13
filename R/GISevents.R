61f151d0c6815b1706d345d8ea1fffc1c92b60f0
Eirik Myrvoll-Nilsen <eirikmn91@gmail.com>
2021-09-13 19:37:56 +0200

    Trimmed GISevents data for our use

--- R/GISevents.R
+++ R/GISevents.R
@@ -3,12 +3,11 @@
 #' A data set containing the onsets of climatic events as recorded in the NGRIP ice core records.
 #'
 #'
-#' @format A data.frame with 117 rows and 5 columns:
+#' @format A data.frame with 117 rows and 4 columns:
 #' \describe{
 #'   \item{Event}{Description of event}
 #'   \item{NGRIP.depth..m.}{Recorded depth in NGRIP (meter)}
 #'   \item{Age..a.b2k}{Corresponding GICC05 age in years before 2000 AD}
-#'   \item{Event.definition.uncertainty}{The dating uncertainty arising from the layer counting process.}
 #'   \item{Maximum.counting.error..years.}{The maximum counting error of each climate event (half the number of uncertain layers).}
 #'   }
 #' @references Will be added soon!
