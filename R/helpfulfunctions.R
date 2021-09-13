34452b25511cd1b8e5ae05c3be4e7b388d8c4e7b
Eirik Myrvoll-Nilsen <eirikmn91@gmail.com>
2021-09-13 20:04:52 +0200

    Fixed bug in helpfulfunctions.R

--- R/helpfulfunctions.R
+++ R/helpfulfunctions.R
@@ -72,7 +72,7 @@ which.index = function(events, record){ ## Finds which indices of 'record' that 
   return(eventindexes)
 }
 
-bremla_simulationsummarizer = function(data,print.progress=FALSE){
+bremla_simulationsummarizer = function(object,print.progress=FALSE){
   if(print.progress) cat("Computing posterior marginal mean and 95% hpd intervals from chronology samples...\n",sep="")
   time.start = Sys.time()
   n = dim(object$simulation$age)[1]
